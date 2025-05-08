197f5db683871c4a4fe3fd255cfd52a9bfeeabc3
Mukul120 <ranamukulsinh@gmail.com>
2025-05-08 11:38:25 +0530

    solved

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://quick-connect-dcml.onrender.com/",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://quick-connect-dcml.onrender.com/api",
     withCredentials: true
 })
 
